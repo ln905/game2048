b755dcbd06087ec135627c7d14ed1321d9dfe56f
ln905 <80632340+ln905@users.noreply.github.com>
2021-06-20 07:46:29 +0700

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -78,11 +78,11 @@ function hidethenumber() {
             squares[i].style.fontSize = '9vh'
         }else if (squares[i].innerHTML==1024){
             squares[i].style.backgroundColor='rgb(236, 200, 80)'
-            squares[i].style.color = 'rgb(237, 197, 63)'
+            squares[i].style.color = 'rgb(249, 246, 241)'
             squares[i].style.fontSize = '6vh'
         } else if (squares[i].innerHTML==2048){
             squares[i].style.backgroundColor='rgb(245, 199, 44)'
-            squares[i].style.color = 'rgb(237, 197, 63)'
+            squares[i].style.color = 'rgb(249, 246, 241)'
             squares[i].style.fontSize = '6vh'
         } 
     }
